6e7ae0d702b92f38375bfd822146d10ad45dcda2
Manfre-98 <62291576+Manfre-98@users.noreply.github.com>
2021-11-25 10:19:21 +0100

    Add files via upload

---
+++ Sprint1/sensors/outsonar/OutSonarCallback.kt
@@ -0,0 +1,24 @@
+package outsonar
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
+import java.lang.Exception
+import org.eclipse.paho.client.mqttv3.MqttCallback
+import org.eclipse.paho.client.mqttv3.MqttMessage
+
+class OutSonarCallback(adapter : OutSonarAdapter) : MqttCallback {
+    private var sonarAdapter: OutSonarAdapter = adapter
+    
+    override fun connectionLost(throwable: Throwable?) {
+        println("Connection to MQTT broker lost!")
+    }
+ 
+
+    override fun messageArrived(s: String?, mqttMessage: MqttMessage) {
+        println("""Message received:${String(mqttMessage.getPayload())}""")
+        sonarAdapter.updateDistance(String(mqttMessage.getPayload()))
+    }
+
+    override fun deliveryComplete(iMqttDeliveryToken: IMqttDeliveryToken?) {
+        // not used
+    }
+}
---
+++ Sprint1/sensors/outsonar/OutSonarController.kt
@@ -0,0 +1,15 @@
+package Sonar
+
+import java.util.concurrent.TimeUnit
+import main.SensorFactory
+import main.SensorPort
+
+class SonarController{
+	lateinit var outsonar : SensorPort
+	lateinit var sensorFactory : SensorFactory
+}
+
+fun main(){
+	System.out.println("merda")
+}
+
