5038b3118ad2d7695c09b2cb6725eba584980bf6
Manfre-98 <62291576+Manfre-98@users.noreply.github.com>
2021-11-30 10:35:30 +0100

    Delete Sprint1/outsonar directory

--- Sprint1/outsonar/resourcealone.pl
+++
@@ -1,7 +0,0 @@
-%====================================================================================
-% resourcealone description   
-%====================================================================================
-context(ctxresource, "127.0.0.1",  "TCP", "8048").
- qactor( resource, ctxresource, "it.unibo.resource.Resource").
-tracing.
-msglogging.
